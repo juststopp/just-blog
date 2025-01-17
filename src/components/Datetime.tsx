d0a106647d7deb259b341a92baa8e692f4bd008d
JustStop__ <contact@juststop.dev>
2025-01-17 15:12:17 +0100

    Updated author display

--- src/components/Datetime.tsx
+++ src/components/Datetime.tsx
@@ -85,24 +85,7 @@ const EditPost = ({ url }: EditPostProps) => {
   return (
     <>
       <span aria-hidden="true"> | </span>
-      <a
-        className="space-x-1.5 hover:opacity-75"
-        href={url}
-        rel="noopener noreferrer"
-        target="_blank"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="icon icon-tabler icons-tabler-outline icon-tabler-edit inline-block !scale-90 fill-skin-base"
-          aria-hidden="true"
-        >
-          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-          <path d="M7 7h-1a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-1" />
-          <path d="M20.385 6.585a2.1 2.1 0 0 0 -2.97 -2.97l-8.415 8.385v3h3l8.385 -8.415z" />
-          <path d="M16 5l3 3" />
-        </svg>
-        <span className="text-base italic">Go to challenge</span>
-      </a>
+      <span className="text-base italic">By {url}</span>
     </>
   );
 };
